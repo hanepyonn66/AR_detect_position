19553dc4a3fdf8c265826a0442133a13c0b27df4
hanepyonn66 <168075316+hanepyonn66@users.noreply.github.com>
2024-05-07 14:15:03 +0900

    kopipe

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-document.getElementById("request_permission").addEventListener("click", function(){
-  if (
-  DeviceMotionEvent &&
-  DeviceMotionEvent.requestPermission &&
-  typeof DeviceMotionEvent.requestPermission === 'function'
-  ) {
-  DeviceMotionEvent.requestPermission();
+var add = function(){
+  var title = document.getElementById("title").value;
+  var label = document.querySelector('.instructions');
+
+  if( navigator.geolocation )
+  {
+      navigator.geolocation.getCurrentPosition(
+
+          function( position )
+          {
+              var location = position.coords ;
+              lat = location.latitude ;
+              lng = location.longitude ;
+
+              saveData(title,lat,lng);
+              addData(title,lat,lng);
+          },
+          function( error )
+          {
+              var errorInfo = [
+                  "unexpected error" ,
+                  "prihibited" ,
+                  "signal blocked" ,
+                  "timeout"
+              ] ;
+              var errorNo = error.code ;
+              var errorMessage = "[error : " + errorNo + "]\n" + errorInfo[ errorNo ];
+
+              label.innerText = errorMessage;
+          } ,
+          {
+              // ここtrueにすると精度が上がる
+              "enableHighAccuracy": false,
+              "timeout": 8000,
+              "maximumAge": 30000,
+          }
+      ) ;
   }
-  if (
-  DeviceOrientationEvent &&
-  DeviceOrientationEvent.requestPermission &&
-  typeof DeviceOrientationEvent.requestPermission === 'function'
-  ) {
-  DeviceOrientationEvent.requestPermission();
+  else
+  {
+      var errorMessage = "Don't Compatible with GeoLocation API" ;
+      label.innerText = errorMessage;
   }
-  })
+}; 
