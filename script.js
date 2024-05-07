e8484f3647ef8cfa3224e513d75cf6a79474947d
hanepyonn66 <168075316+hanepyonn66@users.noreply.github.com>
2024-05-07 13:46:21 +0900

    test

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-document.write("<h1>Hello World</h1>");
+document.getElementById("request_permission").addEventListener("click", function(){
+  if (
+  DeviceMotionEvent &&
+  DeviceMotionEvent.requestPermission &&
+  typeof DeviceMotionEvent.requestPermission === 'function'
+  ) {
+  DeviceMotionEvent.requestPermission();
+  }
+  if (
+  DeviceOrientationEvent &&
+  DeviceOrientationEvent.requestPermission &&
+  typeof DeviceOrientationEvent.requestPermission === 'function'
+  ) {
+  DeviceOrientationEvent.requestPermission();
+  }
+  })
