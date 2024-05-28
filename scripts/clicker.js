224849f00783a29687b0f5a135d04aa4c0a8a0bd
hanepyonn66 <168075316+hanepyonn66@users.noreply.github.com>
2024-05-28 10:19:12 +0900

    　

--- scripts/clicker.js
+++ scripts/clicker.js
@@ -1,9 +1,10 @@
 AFRAME.registerComponent('click_pin_id1', {
     init: function () {
-      this.el.addEventListener('click', function () {
-        // クリック時の処理をここに記述
-        const explain_id1 = document.getElementById("explain_id1");
-        explain_id1.setAttribute('visible','true')
-      });
+        this.el.addEventListener('click', function () {
+            // クリック時の処理をここに記述
+            const explain_id1 = document.getElementById("explain_id1");
+            const isVisible = explain_id1.getAttribute('visible');
+            explain_id1.setAttribute('visible', !isVisible);
+        });
     }
-  });
+});
